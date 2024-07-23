8a096dfb044ebc6a73a9801af761cf3eec3d7430
TonyCg10 <103067004+TonyCg10@users.noreply.github.com>
2024-07-22 21:46:40 -0400

    1

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-full justify-center items-center">
+      <div className="text-red-600 text-4xl">Tutorial</div>
+    </div>
+  )
+}
+
+export default App
